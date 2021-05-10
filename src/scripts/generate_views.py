712fd0fe4eabacf8e33ea4f7347e8b19cab53e95
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-05-10 10:21:32 -0400

    move scripts folder

---
+++ src/scripts/generate_views.py
@@ -0,0 +1,93 @@
+import os
+import asyncio
+import databases
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+BIKESHARE_DATABASE_URL = os.environ.get("BIKESHARE_DATABASE_URL")
+
+
+materialized_views = [
+    """
+    -- TURN RAW TRIP TABLE INTO SIMPLIFIED TABLE
+    create materialized view if not exists trips_simplified as
+        select
+            start_station, end_station,
+            extract(year from start_time::timestamp) as y,
+            extract(month from start_time::timestamp) as m,
+            extract(day from start_time::timestamp) as d,
+            extract(hour from start_time::timestamp) as h
+        from trips;
+    """,
+    """
+    -- ROLL UP SIMPLIFIED TABLE INTO YEAR/QUARTER
+    create materialized view if not exists  trips_by_quarter as
+        select start_station, end_station, y,
+            case when m < 4 then 0.1
+            when m < 7 then 0.2
+            when m < 10 then 0.3
+            else 0.4 end as q,
+            count(*) as trips
+        from trips_simplified
+        group by start_station, end_station, y, q;
+    """,
+]
+
+
+query_template = """
+    with origins as (
+    select
+        start_station as station_id,
+        count(*) as origins
+        from trips_simplified
+        where end_station = STATION_ID
+        group by start_station
+    ),
+    destinations as (
+    select
+        end_station as station_id,
+        count(*) as destinations
+        from trips_simplified
+        where start_station = STATION_ID
+        group by end_station
+    ),
+    numeric_data as (
+        select o.station_id, o.origins, d.destinations
+        from origins o
+        full outer join destinations d on o.station_id = d.station_id
+    )
+    select ss.geom, ss.name, nd.* from station_shapes ss
+    inner join numeric_data nd on ss.id = nd.station_id
+"""
+
+
+async def main():
+
+    db = databases.Database(BIKESHARE_DATABASE_URL)
+
+    await db.connect()
+
+    for view_q in materialized_views:
+        await db.execute(view_q)
+
+    # ids = await db.fetch_all("SELECT DISTINCT id FROM station_shapes ORDER BY id asc")
+
+    # for id in ids:
+    #     station_id = id["id"]
+
+    #     query = f"""
+    #         CREATE TABLE station_{station_id} AS
+    #     """ + query_template.replace(
+    #         "STATION_ID", str(station_id)
+    #     )
+
+    #     # print(query)
+
+    #     await db.execute(query=query)
+
+    # print("Completed", station_id)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
